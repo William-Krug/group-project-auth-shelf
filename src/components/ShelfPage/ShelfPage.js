fe8e56d735138163c98e973cc9af3ae53fa2bb55
Dusty Meyers <dustymeyerssculpture@gmail.com>
2021-03-12 11:00:01 -0600

    deleteItem swal setup

--- src/components/ShelfPage/ShelfPage.js
+++ src/components/ShelfPage/ShelfPage.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import ShelfPageForm from '../ShelfPageForm/ShelfPageForm';
 import { useDispatch, useSelector } from 'react-redux';
+import swal from 'sweetalert';
 
 function ShelfPage() {
   const shelfItems = useSelector(store => store.shelfItems);
@@ -16,6 +17,25 @@ function ShelfPage() {
   // handles delete button click
   const deleteItem = (itemId) => {
     console.log('item to delete:', itemId);
+    // sweet alert will first check that the user wants to delete the item. 
+    swal({
+      title: "Are you sure?",
+      text: "Once deleted, you will not be able to recover this imaginary file!",
+      icon: "warning",
+      buttons: true,
+      dangerMode: true,
+    })
+    // willDelete is kitty cat, first argument of the promise is a boolean when Okay is clicked
+    .then((willDelete) => {
+      console.log('willDelete', willDelete);
+      if (willDelete) {
+        swal("Poof! Your imaginary file has been deleted!", {
+          icon: "success",
+        });
+      } else {
+        swal("Your imaginary file is safe!");
+      }
+    });
   }
   
   return (
